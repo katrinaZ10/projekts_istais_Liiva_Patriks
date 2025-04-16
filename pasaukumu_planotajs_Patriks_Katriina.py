68bd7a7912460cc31dca7fe5af3d89c99843465b
katrinaZ10 <katrina.zundmane@svg.lv>
2025-04-16 08:29:42 +0300

    skolotāja salaboja izveleto datumu

--- pasaukumu_planotajs_Patriks_Katriina.py
+++ pasaukumu_planotajs_Patriks_Katriina.py
@@ -26,11 +26,10 @@ def aprekinat_biletes(skaits, veids): #funkcija ar padotajiem parametriem (ko ie
 def brivas_vietas(kas): # funkcija atgriež cik brīvas vientas ir izvēlētajā pasākumā
     return(ietilpiba[kas])
         
-def laiks():
+def dienas_lidz_pasakumam(datums):
     sodien = date.today()
-    datums = date('')
-    diff = datums - sodien
-    print(diff.days)
+    diff = (datums - sodien).days
+    return diff
 
 
 print('Sveicināti pasākumu plānotājā!')
@@ -38,61 +37,71 @@ print('Sveicināti pasākumu plānotājā!')
 try:
     while True:
         print('Pasākumi veidi: ', '\n1 - Koncerts', '\n2 - Festivals', '\n3 - Sacencibas')
-        veids = int(input('Ievadiet kuru no pasākumiem apmeklēsiet (1, 2 vai 3): '))
+        ievade = int(input('Ievadiet kuru no pasākumiem apmeklēsiet (1, 2 vai 3): '))
 
-        if veids == 1:
+        if ievade == 1:
             veids = 'koncerts'
             break
-        elif veids == 2:
+        elif ievade == 2:
             veids = 'festivals'
             break
-        elif veids == 3: 
+        elif ievade == 3: 
             veids = 'sacencibas'
             break
-        elif veids >= 4:
+        else:
             print('Ievadiet no 1 līdz 3 ciparu!')
-            break
-        
-    
+
     print('Pieejami datumi : ')
     for i,dat in enumerate(datumi[veids],1): #cikls konsolē parādīs pieejamos datumus
         print(f"{i} - {dat}")
 
-    liet_datums = input('Ievadiet sev vēlamo datumu: ')
-    if veids == 'koncers' and liet_datums == '1':
-        izveletais_dat = ('Datums ',datumi.get('koncerts'[1]))
-    elif veids == 'koncers' and liet_datums == '2':
-        izveletais_dat = date(2025, 7, 21)
-        print('Koncerts 2')
-    elif veids == 'koncers' and liet_datums == '3':
-        izveletais_dat = date(2025, 10, 16)
-        print('Koncerts 3')
+    liet_datums = int(input('Ievadiet sev vēlamo datumu: '))
+    #validāciju ievadītajam datumam(1,2,3)
 
-    elif veids == 'festivals' and liet_datums == '1':
-        izveletais_dat = date(2025, 6, 6)
-        print('Festivāls 1')
-    elif veids == 'festivals' and liet_datums == '2':
-        izveletais_dat = date(2025, 6, 28)
-        print('Festivāls 2')
-    elif veids == 'festivals' and liet_datums == '3':
-        izveletais_dat = date(2025, 7, 13)
-        print('Festivāls 3')
-    elif veids == 'festivals' and liet_datums == '4':
-        izveletais_dat = date(2025, 8, 5)
-        print('Festivāls 4')
-
+    if 1<=liet_datums<=len(datumi[veids]):
+        izveletais_dat=datumi[veids][liet_datums-1] 
+    else:
+        print('Ievadiet pieejamu datuma kārtas numuru!')
+        exit()
 
     print('Pieejamo biļešu skaits: ', brivas_vietas(veids)) 
 
     bilesu_sk = int(input('Cik biļetes vēlaties: '))
 
+    if bilesu_sk > ietilpiba[veids]:
+        print('Nav pietikama pasākuma ietilpība!')
+
     print('-'*17) #Konsolē parāda "čeku"
     print('Pasākuma veids: ', veids)
     print('Datums: ', izveletais_dat)
-    print('Dienas līdz pasākumam: ')
+    print('Dienas līdz pasākumam: ',dienas_lidz_pasakumam(izveletais_dat))
     print('Kopējā biļēšau cena: ', aprekinat_biletes(bilesu_sk, veids))
     print('\nPaldies par iepirkšanos!')
 
 except ValueError:
             print('Lūdzu ievadiet skaitli!')
 
+
+#pārbaude vai neparsniedz pieejamo bilesu skaitu
+'''if veids == 'koncers' and liet_datums == '1':
+        izveletais_dat = date(2025, 6, 12)
+    elif veids == 'koncers' and liet_datums == '2':
+        izveletais_dat = date(2025, 7, 21)
+    elif veids == 'koncers' and liet_datums == '3':
+        izveletais_dat = date(2025, 10, 16)
+
+    elif veids == 'festivals' and liet_datums == '1':
+        izveletais_dat = date(2025, 6, 6)
+    elif veids == 'festivals' and liet_datums == '2':
+        izveletais_dat = date(2025, 6, 28)
+    elif veids == 'festivals' and liet_datums == '3':
+        izveletais_dat = date(2025, 7, 13)
+    elif veids == 'festivals' and liet_datums == '4':
+        izveletais_dat = date(2025, 8, 5)
+
+    elif veids == 'sacencibas' and liet_datums == '1':
+        izveletais_dat = date(2025, 7, 30)
+    elif veids == 'sacencibas' and liet_datums == '2':
+        izveletais_dat = date(2025, 9, 2)
+    elif veids == 'sacencibas' and liet_datums == '3':
+        izveletais_dat = date(2025, 11, 17)'''
