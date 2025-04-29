b310cb2787fb312aa352d23133b13ad54e868307
Rullis-ux <patriks.rullis@svg.lv>
2025-04-29 09:41:36 +0300

    kkas
    a

--- pasaukumu_planotajs_Patriks_Katriina.py
+++ pasaukumu_planotajs_Patriks_Katriina.py
@@ -1,5 +1,25 @@
 from datetime import date #importē datime, lai var aprēķināt cik dienas līdz festivālam
 
+<<<<<<< Updated upstream
+=======
+koncerts1 = date(2025, 6, 12)
+koncerts2 = date(2025, 7, 21)
+koncerts3 = date(2025, 10, 16)
+
+#[date(2025, 6, 6), date(2025, 6, 28), date(2025, 7, 13), date(2025, 8, 5)]
+'''festivals1 : date(2025, 6, 6),
+        festivals2 : date(2025, 6, 28),
+        festivals3 : date(2025, 7, 13),
+        festivals4 : date(2025, 8, 5)'''
+
+festivals1 = '6. jūnijs'
+festivals2 = '28. jūnijs'
+festivals3 = '13. jūlijs'
+festivals4 = '5. augusts'
+
+
+
+>>>>>>> Stashed changes
 datumi = { #vārdnīca ar pasākumu datumiem
     'koncerts' : [date(2025, 6, 12), date(2025, 7, 21) ,date(2025, 10, 16)],
     'festivals': [date(2025, 6, 6), date(2025, 6, 28), date(2025, 7, 13), date(2025, 8, 5)],
@@ -17,7 +37,12 @@ cena = { #vārdnīca ar katra pasākuma biļetes cenu
     'festivals': 8,
     'sacencibas' : 12
 }
+class Rekini():
+    def aprekinat_biletes(skaits, veids): #funkcija ar padotajiem parametriem (ko ievada lietotājs), atgriež kopējo cenu
+        biletes_cena_kopa = skaits*cena[veids]
+        return biletes_cena_kopa
 
+<<<<<<< Updated upstream
 
 def aprekinat_biletes(skaits, veids): #funkcija ar padotajiem parametriem (ko ievada lietotājs), atgriež kopējo cenu
     biletes_cena_kopa = (skaits)*cena[veids]
@@ -30,6 +55,20 @@ def dienas_lidz_pasakumam(datums):
     sodien = date.today()
     diff = (datums - sodien).days
     return diff
+=======
+    def brivas_vietas(veids):
+        return ietilpiba[veids]
+        
+    def laiks():
+        from datetime import date
+
+        sodien = date.today()
+        datums = date(2025, 7, 30)
+        diff = datums - sodien
+        print(diff.days)
+
+
+>>>>>>> Stashed changes
 
 
 print('Sveicināti pasākumu plānotājā!')
@@ -52,6 +91,7 @@ try:
             print('Ievadiet ciparu no 1 līdz 3!')
             print('-'*17)
 
+<<<<<<< Updated upstream
     while True:
         print('Pieejami datumi : ')
         for i,dat in enumerate(datumi[veids],1): #cikls konsolē parādīs pieejamos datumus
@@ -59,6 +99,11 @@ try:
 
         liet_datums = int(input('Ievadiet sev vēlamo datumu: '))
         #validāciju ievadītajam datumam(1,2,3)
+=======
+        #print(f'{veids} pieejamie datumI: ','\n1  - [veids]')
+    #for i in datumi[veids]:
+    print(datumi[veids])
+>>>>>>> Stashed changes
 
         if 1<=liet_datums<=len(datumi[veids]):
             izveletais_dat=datumi[veids][liet_datums-1]
@@ -70,6 +115,7 @@ try:
     while True:
         print('Pieejamo biļešu skaits: ', brivas_vietas(veids)) 
 
+<<<<<<< Updated upstream
         bilesu_sk = int(input('Cik biļetes vēlaties: '))
 
         if bilesu_sk > ietilpiba[veids]:
@@ -115,3 +161,9 @@ print('\nPaldies par iepirkšanos!')
         izveletais_dat = date(2025, 9, 2)
     elif veids == 'sacencibas' and liet_datums == '3':
         izveletais_dat = date(2025, 11, 17)'''
+=======
+    print( 'Kopējā biļešu cena: ', aprekinat_biletes(bilesu_sk, veids), 'EUR')
+   
+except ValueError:
+        print('Lūdzu ievadiet skaitli!')
+>>>>>>> Stashed changes
